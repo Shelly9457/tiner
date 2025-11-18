48d452d9362adc04d0bcfaf73773cbaada916e49
shelly <shelly9457@gmail.com>
2025-11-18 10:21:55 +0800

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://https://tiner-self.vercel.app/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://https://tiner-self.vercel.app/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://https://tiner-self.vercel.app/generation',
+            lastModified: new Date(),
+        },
+    ]
+}
